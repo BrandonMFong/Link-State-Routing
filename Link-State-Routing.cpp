b2873d27650bcfe0e9a19c7759b543a2002c7ba7
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-24 13:24:50 -0700

    init

---
+++ Link-State-Routing.cpp
@@ -0,0 +1,16 @@
+// Link-State-Routing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>      // std::ifstream
+#include "json.hpp" // Use when it is ready to be parameterized
+
+// Priority is to establish the algorithm then do the configuration 
+
+using json = nlohmann::json;
+using ifstream = std::ifstream;
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
